6e146f2b42848d8c41156834567530ac41d86876
Nem-Ajvaz <45509371+Nem-Ajvaz@users.noreply.github.com>
2021-11-09 13:23:37 +1100

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Orderly", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/orderly", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
